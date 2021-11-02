2405925e9a3de69fc619e4c4fb005f976261f30a
Johnkai2196 <johnkaic@metropolia.fi>
2021-11-02 11:15:47 +0200

    added delete passwork

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,12 +3,20 @@
 const {users,getUser} = require('../models/userModel');
 
 const user_list_get = (req, res) => {
+  // another style making a new array
+  const newUser =users.map((user)=>{
+    delete user.password;
+  })
+
   res.json(users);
 };
 const user_get = (req, res) => {
   const user = getUser(req.params.userId);
+  //working
+  delete user.password;
   res.json(user);
 };
+
 module.exports = {
   user_list_get,
   user_get,
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -5,4 +5,5 @@ const {user_list_get, user_get} = require('../controllers/userController');
 const router = express.Router();
 router.get('/', user_list_get);
 router.get('/:userId',user_get);
+
 module.exports = router;
