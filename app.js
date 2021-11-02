ee02f0e1ddede5bb000ff129c3f966a0a26d8966
Johnkai2196 <johnkaic@metropolia.fi>
2021-11-02 09:16:48 +0200

    added post,put and delete

--- app.js
+++ app.js
@@ -7,4 +7,17 @@ app.get('/cat', (req, res) => {
   res.send('From this endpoint you can get cats.')
 });
 
+app.get('/cat/:catId',(req,res)=>{
+  console.log('/cat route',req.params)
+  res.send(`Form this endpoint you can get a specific cat. The catID is ${req.params.catId}`)
+})
+app.post('/cat', (req, res) => {
+  res.send('From this endpoint you can add cats.')
+});
+app.put('/cat', (req, res) => {
+  res.send('From this endpoint you can put cats.')
+});
+app.delete('/cat', (req, res) => {
+  res.send('From this endpoint you can delete cats.')
+});
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
