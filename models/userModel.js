6b04fbde2a83f0a7e84afa93e73b4def1aaf832d
Johnkai2196 <johnkaic@metropolia.fi>
2021-11-16 11:05:01 +0200

    added branch week-4

--- models/userModel.js
+++ models/userModel.js
@@ -70,10 +70,22 @@ const updateUser = async (user, next) => {
   }
 };
 
+const getUserLogin = async (params) => {
+  try {
+    console.log(params);
+    const [rows] = await promisePool.execute(
+        'SELECT * FROM wop_user WHERE email = ?;',
+        params);
+    return rows;
+  } catch (e) {
+    console.log('error', e.message);
+  }
+};
 module.exports = {
   getAllUsers,
   getUser,
   insertUser,
   deleteUser,
   updateUser,
+  getUserLogin,
 };
